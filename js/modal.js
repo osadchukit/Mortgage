cecdeb10c2ca4a8c93b079bc0f1fca40b8306ae7
Ihor Statsenko <statsenkoin@gmail.com>
2022-12-10 11:20:01 +0200

    root markup

---
+++ js/modal.js
@@ -0,0 +1,4 @@
+const rootRef = document.querySelector('#root');
+const markup = `<div class="bank-box"></div>
+      <div class="bank-info"></div>`;
+rootRef.insertAdjacentHTML('beforeend', markup);
